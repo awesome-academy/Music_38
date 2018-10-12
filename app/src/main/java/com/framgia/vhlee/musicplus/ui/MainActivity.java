2ccbb8e00af3c9c9ce0861ed46971fcdef24f590
Vo Huu Ly <vhlee1996@gmail.com>
2018-10-12 14:00:07 +0700

    refs2634 create main layout

--- app/src/main/java/com/framgia/vhlee/musicplus/ui/MainActivity.java
+++ app/src/main/java/com/framgia/vhlee/musicplus/ui/MainActivity.java
@@ -1,15 +1,60 @@
 package com.framgia.vhlee.musicplus.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.framgia.vhlee.musicplus.R;
+import com.framgia.vhlee.musicplus.ui.home.HomeFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    /**
+     * Navigation: item click event
+     *
+     * @param menuItem
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                selectTab(HomeFragment.newInstance(), null, true);
+                break;
+            case R.id.navigation_my_music:
+                break;
+            case R.id.navigation_settings:
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private void initUI() {
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+        selectTab(HomeFragment.newInstance(), null, true);
+    }
+
+    private void selectTab(Fragment fragment, String tag, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_fragments_container, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(tag);
+        }
+        transaction.commit();
     }
 }
